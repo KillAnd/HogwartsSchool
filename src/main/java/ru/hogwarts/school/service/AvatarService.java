b5a944ae008b9f039394aa7b8d892ab2653d391d
kila1 <kila14081996@gmail.com>
2024-09-08 21:08:21 +0300

    Hogwarts school part #9, logged and configs added!

--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -22,6 +24,7 @@ public class AvatarService {
     private final int BUFFER_SIZE = 1024;
     private final StudentRepository studentRepository;
     private final AvatarRepository avatarRepository;
+    Logger logger = LoggerFactory.getLogger(AvatarService.class);
 
     @Value("${path.to.avatars.folder}")
     private String avatarsDir;
@@ -31,6 +34,7 @@ public class AvatarService {
     }
 
     public void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
+        logger.info("Был запущен метод по загрузке аватара студента " + studentId);
         Student student = studentRepository.findById(studentId).orElseThrow(() ->
                 new IllegalArgumentException("Студент с id " + studentId + " отсутствует в базе данных"));
 
@@ -40,6 +44,7 @@ public class AvatarService {
     }
 
     public Path saveToLocalDirectory(Student student, MultipartFile avatarFile) throws IOException {
+        logger.info("Аватар студента добавлен в локальную директорию");
         Path avatarPath = Path
                 .of(avatarsDir, student + "." + getExtensions(avatarFile.getOriginalFilename()));
         Files.createDirectories(avatarPath.getParent());
@@ -57,6 +62,7 @@ public class AvatarService {
     }
 
     public Avatar saveToDataBased(Student student, Path avatarPath, MultipartFile avatarFile) throws IOException{
+        logger.info("Аватар добавлен в базу данных");
         Avatar avatar = getAvatarStudent(student);
         avatar.setStudent(student);
         avatar.setFilePath(avatarPath.toString());
@@ -68,6 +74,7 @@ public class AvatarService {
     }
 
     public Avatar getAvatarStudent(Student student) {
+        logger.info("Был запущен метод по получению аватара из базы данных");
         return avatarRepository.findByStudent(student).orElseGet(() -> {
             Avatar avatar = new Avatar();
             avatar.setStudent(student);
@@ -76,14 +83,17 @@ public class AvatarService {
     }
 
     public Avatar findAvatar(long id) {
+        logger.info("Был запущен метод по получению аватара из базы данных по идентификатору id" + id);
         return avatarRepository.findById(id).orElseThrow(() ->
                 new IllegalArgumentException("Студент с id " + id + " отсутствует в базе данных"));
     }
     private String getExtensions(String fileName) {
+        logger.info("Извлечение расширения файла");
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 
     public List<Avatar> findAllAvatars(Integer pageNumber, Integer pageSize) {
+        logger.info("Вызван метод получения всех аватарок с использованием пагинации");
         PageRequest pageRequest = PageRequest.of(pageNumber - 1, pageSize);
         return avatarRepository.findAll(pageRequest).getContent();
     }
--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
@@ -11,20 +13,26 @@ import java.util.List;
 public class FacultyService {
 
     private final FacultyRepository facultyRepository;
+    Logger logger = LoggerFactory.getLogger(FacultyService.class);
 
     public FacultyService(FacultyRepository facultyRepository) {
         this.facultyRepository = facultyRepository;
     }
 
     public Faculty createFaculty(Faculty faculty) {
+        logger.info("Был создан новый факультет");
         return facultyRepository.save(faculty);
     }
 
     public Faculty readFaculty(long id) {
+        logger.info("Был запущен метод по получению факультета");
+        logger.error("Не правильно указан id факультета");
         return facultyRepository.findById(id).orElse(null);
     }
 
     public Faculty updateFaculty(long id, Faculty faculty) {
+        logger.info("Был изменен факультет");
+        logger.error("Не был указан id факультета");
         return facultyRepository.findById(id).map(faculty1 -> {
             faculty1.setName(faculty.getName());
             faculty1.setColor(faculty.getColor());
@@ -34,6 +42,8 @@ public class FacultyService {
     }
 
     public Faculty deleteFaculty(long id) {
+        logger.info("Был удален факультет");
+        logger.error("Не был указан id факультета");
         return facultyRepository.findById(id).map(faculty -> {
             facultyRepository.deleteById(id);
             return faculty;
@@ -41,17 +51,24 @@ public class FacultyService {
     }
 
     public List<Faculty> allListFaculty() {
+        logger.info("Был запущен метод вызова всех факультетов");
         return facultyRepository.findAll();
     }
 
     public List<Faculty> filterByColor(String color) {
+        logger.info("Запущен метод фильтрации факультетов по цвету");
+        logger.error("Не был указан цвет факультета");
         return facultyRepository.findAllByColor(color);
     }
     public List<Faculty> findByColorOrName(String name, String color) {
+        logger.info("Запущен метод по поиску факультетов по цвету или имени");
+        logger.error("Неверно указан один из параметров");
         return facultyRepository.findAllByNameIgnoreCaseOrColorIgnoreCase(name ,color);
     }
 
     public List<Student> findStudentByFaculty(long id) {
+        logger.info("Запущен метод по поиску студентов из факультета " + id);
+        logger.error("Неверно указан id");
         return facultyRepository.findById(id)
                 .map(Faculty::getStudents)
                 .orElse(null);
